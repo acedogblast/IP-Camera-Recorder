c6f75c545c3181066e25baad3a1a95540f12c9b5
acedogblast <charles2000wang@gmail.com>
2021-04-26 16:07:38 -0400

    Initial code

---
+++ Test.sh
@@ -0,0 +1,1 @@
+ffmpeg -i "rtsp://192.168.1.41:554/h264?username=admin&password=123456" -an -c:v copy "output.mkv"
--- src/com/wang/ip_camera/Program.java
+++ src/com/wang/ip_camera/Program.java
@@ -1,7 +1,21 @@
 package com.wang.ip_camera;
 
 public class Program {
+
+    private String outputPath = "/home/charles/";
+
+
     public static void main(String[] args) {
-        
+        System.out.println("Starting IP camera recorder.");
+        run();
+    }
+
+    private static void run() {
+        try {
+            Runtime run = Runtime.getRuntime();
+            Process FFmpeg = run.exec("ffmpeg -i \"rtsp://192.168.1.41:554/h264?username=admin&password=123456\" -an -c:v copy \"output.mkv\"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
